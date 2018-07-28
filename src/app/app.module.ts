ec28c282dcff4a00a51a21a8f04d2eec0de9ddef
dacnoi <mr.dacnoi@gmail.com>
2018-07-28 16:05:48 +0700

    dacnoi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+/**
+ * author : noipd
+ * email : noipd@fpt.com.vn
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppRoutingModule} from './app-routing.module';
+import { HttpClientModule} from '@angular/common/http';
+
+import { LoginComponent } from './login/login.component'
+import { InforDetailComponent } from './infor-detail/infor-detail.component';
+
+import { AppService } from './app.service';
+import { OAuthService} from 'angular2-oauth2';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InforDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+     
   ],
-  providers: [],
+  providers: [
+    AppService,
+    OAuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
